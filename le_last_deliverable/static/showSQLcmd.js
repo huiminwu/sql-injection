71410f8412f80688b5c9022235fa4938549a0f57
th0mazzz <thomaszhao2001@gmail.com>
2019-04-21 16:42:16 -0400

    ooh hovering thingies

--- le_last_deliverable/static/showSQLcmd.js
+++ le_last_deliverable/static/showSQLcmd.js
@@ -1,3 +1,12 @@
+//INFORMATION PAGE HOVER OVER
+var consequenceuno = document.getElementById("consequence-1");
+var consequencedos = document.getElementById("consequence-2");
+var consequencetres = document.getElementById("consequence-3");
+var consequencecuatro = document.getElementById("consequence-4");
+var consequencecinco = document.getElementById("consequence-5");
+var consequenceseis = document.getElementById("consequence-6");
+
+// THE DEMO
 var username = document.getElementById("user");
 var password = document.getElementById("pass");
 var usernameinput = document.getElementById("usernameinput");
@@ -7,7 +16,7 @@ usernameinput.addEventListener("input", function(e){
     console.log(e);
     username.innerHTML = usernameinput.value
 });
-			       
+
 passwordinput.addEventListener("input", function(e){
     password.innerHTML = passwordinput.value
 });
